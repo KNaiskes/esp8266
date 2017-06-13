f40eb02cec368179cadaaf000dcca497808fedaa
Kiriakos <kiriakosnaiskes@gmail.com>
2017-06-14 00:56:21 +0300

    first commint, added main.py and readme files

---
+++ README.md
@@ -0,0 +1,1 @@
+My simple MicroPython projects that I develop while I am trying to understand it - leard it
---
+++ main.py
@@ -0,0 +1,16 @@
+from machine import Pin # for testing purpose - will be removed some time later
+
+def connect(ssid,passw):
+	led = Pin(2,Pin.OUT)
+	import network
+	netw = network.WLAN(network.STA_IF)
+	if not netw.isconnected():
+		led.on() # while it is not connected led will be off
+		print("Trying to connect to network:",ssid)
+		netw.active(True)
+		netw.connect(ssid,passw)
+		while not netw.isconnected():
+			pass
+		led.off() # turn led on when it is connected to the network
+
+connect("","") # add your network credentials
