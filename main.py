7713ab91616fdbfa5a417555a84c9b1684fd3b07
Kiriakos <kiriakosnaiskes@gmail.com>
2017-06-16 08:30:25 +0300

    added access point configuration function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from netConfig import *
 
 connect("","") # add your network credentials
+setupAP("","",) # parameters str - str - int
--- netConfig.py
+++ netConfig.py
@@ -1,8 +1,8 @@
 from machine import Pin # for testing purpose - will be removed some time later
+import network
 
 def connect(ssid,passw):
 	led = Pin(2,Pin.OUT)
-	import network
 	netw = network.WLAN(network.STA_IF)
 	if not netw.isconnected():
 		led.on() # while it is not connected led will be off
@@ -12,4 +12,9 @@ def connect(ssid,passw):
 		while not netw.isconnected():
 			pass
 		led.off() # turn led on when it is connected to the network
+	
+def setupAP(ssid,passw,channel):
+	espAP = network.WLAN(network.AP_IF)
+	espAP.config(essid = ssid, password = passw, channel = channel)
+	print("Access point name:",ssid," ,channel:",channel)
 
