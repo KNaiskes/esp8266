ed56cabd8f3ed3cd5da1dbea5bd70e7895c54a77
Kiriakos <kiriakosnaiskes@gmail.com>
2017-07-31 21:01:20 +0300

    added simple form

---
+++ arduinoFramework/controlPanel/main.ino
@@ -0,0 +1,48 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h>
+#include "index.h"
+
+// network
+const char* ssid = "";
+const char* passw = "";
+// credentials of the second network that I usually use 
+const char* ssid2 = "";
+const char* passw2 = "";
+
+const char* setHostname = "";
+
+const int port = 80;
+
+// app
+const char* appName = "";
+const char* appPasw = "";
+
+ESP8266WebServer server(port);
+ESP8266WiFiMulti multinetw;
+
+void handleIndex(){
+	server.send(200,"text/html",HTML_INDEX);
+}
+void handleLogin(){
+	// will fix later
+	if(server.arg("username") == "user1" && server.arg("password")=="test"){
+		server.send(200,"text/html","<h1>Success</h1>");
+	}
+}
+
+
+void setup(){
+	//WiFi.begin(ssid,passw);
+	multinetw.addAP(ssid,passw);
+	WiFi.softAP(appName,appPasw); // default ip: 192.168.4.1
+	WiFi.hostname(setHostname);
+	server.on("/",handleIndex);
+	server.on("/connect",HTTP_POST,handleLogin);
+	server.begin();
+}
+void loop(){
+	server.handleClient();
+}
+
